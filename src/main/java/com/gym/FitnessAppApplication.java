5d81dba1c770e13374e6f0278c13161a64b2ba67
Axmadullo2000 <axmadullo2000@gmail.com>
2026-05-12 01:41:32 +0500

    Gym App 2.0 (added client side, optimised admin side)

--- src/main/java/com/gym/FitnessAppApplication.java
+++ src/main/java/com/gym/FitnessAppApplication.java
@@ -3,10 +3,13 @@ package com.gym;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FitnessAppApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tashkent"));
         SpringApplication.run(FitnessAppApplication.class, args);
     }
 
